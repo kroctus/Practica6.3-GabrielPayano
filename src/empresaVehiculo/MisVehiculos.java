b979d6eb5f1b36a684017caa889aaa5f05cf8ee7
gabriel_payano <gabriel21kun@gmail.com>
2019-03-01 14:42:22 +0100

    "modificamos los métodos para rellenar los arrays"

--- src/empresaVehiculo/MisVehiculos.java
+++ src/empresaVehiculo/MisVehiculos.java
@@ -5,6 +5,9 @@
  */
 package empresaVehiculo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author gabriel
@@ -13,28 +16,47 @@ public class MisVehiculos {
 
     public static void main(String[] args) {
 
-        // la instancia easydrive de la clase EmpresaAlquilerVehiculos
-        EmpresaAlquilerVehiculo easydrive = new EmpresaAlquilerVehiculo("A-28-187189", "easy drive", "www.easydrive.com");
-// registro de los clientes de la empresa
-        easydrive.registrarCliente(new Cliente("X5618927C", "Juan", "González López"));
+//        // la instancia easydrive de la clase EmpresaAlquilerVehiculos
+//        EmpresaAlquilerVehiculo easydrive = new EmpresaAlquilerVehiculo("A-28-187189", "easy drive", "www.easydrive.com");
+//// registro de los clientes de la empresa
+//        easydrive.registrarCliente(new Cliente("X5618927C", "Juan", "González López"));
+//
+//        easydrive.registrarCliente(new Cliente("Z7568991Y", "Luis", "Fernández Gómez"));
+//
+//// registro de los vehículos de la empresa
+//        easydrive.registrarVehiculo(new Vehiculo("4060 TUR", "Skoda", "Fabia", "Blanco", 90.0, false));
+//
+//        easydrive.registrarVehiculo(new Vehiculo("4070 DEP", "Ford", "Mustang", "Rojo", 150.0, true));
+//
+//        easydrive.registrarVehiculo(new Vehiculo("4080 TUR", "VW", "GTI", "Azul", 110.0, false));
+//
+//        easydrive.registrarVehiculo(new Vehiculo("4090 TUR", "SEAT", "Ibiza", "Blanco", 90.0, false));
+//
+//        easydrive.registrarVehiculo(new Vehiculo("4100 FUR", "Fiat", "Ducato", "Azul", 80.0, true));
+//
+//        // imprime la relación de clientes de easydrive
+//        easydrive.imprimirClientes();
+//// imprime el catálogo de vehículos de easydrive
+//        easydrive.imprimirVehiculos();
+//        
+        //Creamos una empresa de alquiler de vehiculos
+        EmpresaAlquilerVehiculo nuevaEmpresa = new EmpresaAlquilerVehiculo("B-85-19657425", "empresa", "www.tuEmprersaDevehiculos.com");
 
-        easydrive.registrarCliente(new Cliente("Z7568991Y", "Luis", "Fernández Gómez"));
+        //Utilizamos un bucle y los respecivos metodos para rellenar los arrays de vehiculos y clientes con 25 veces.
+        int contador = 1;
+        do {
+            Cliente aux = Cliente.clienteAleatorio();
+            nuevaEmpresa.registrarCliente(aux);
+            contador++;
+        } while (contador < 26);
 
-// registro de los vehículos de la empresa
-        easydrive.registrarVehiculo(new Vehiculo("4060 TUR", "Skoda", "Fabia", "Blanco", 90.0, false));
+        contador = 1;
+        do {
 
-        easydrive.registrarVehiculo(new Vehiculo("4070 DEP", "Ford", "Mustang", "Rojo", 150.0, true));
-
-        easydrive.registrarVehiculo(new Vehiculo("4080 TUR", "VW", "GTI", "Azul", 110.0, false));
-
-        easydrive.registrarVehiculo(new Vehiculo("4090 TUR", "SEAT", "Ibiza", "Blanco", 90.0, false));
-
-        easydrive.registrarVehiculo(new Vehiculo("4100 FUR", "Fiat", "Ducato", "Azul", 80.0, true));
-
-        // imprime la relación de clientes de easydrive
-        easydrive.imprimirClientes();
-// imprime el catálogo de vehículos de easydrive
-        easydrive.imprimirVehiculos();
+            Vehiculo aux = Vehiculo.vehiculoAleatorio();
+            nuevaEmpresa.registrarVehiculo(aux);
+            contador++;
+        } while (contador < 26);
 
     }
 
