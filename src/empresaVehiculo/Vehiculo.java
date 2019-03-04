cfabc4f65654c9856d9a8a48cfb11d5fcd3b2c49
gabriel_payano <gabriel21kun@gmail.com>
2019-03-04 16:46:01 +0100

    "Realizamos cambios para adaptar los métodos a las ArrayList"

--- src/empresaVehiculo/Vehiculo.java
+++ src/empresaVehiculo/Vehiculo.java
@@ -5,6 +5,7 @@
  */
 package empresaVehiculo;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -77,108 +78,103 @@ public class Vehiculo {
         this.disponible = disponible;
     }
 
-    public static Vehiculo vehiculoAleatorio() {
-
-        String[] matricula = new String[8];
-
-        matricula[0] = "1234BBB";
-        matricula[1] = "346CFG";
-        matricula[2] = "589UYH";
-        matricula[3] = "358OPL";
-        matricula[4] = "569KPL";
-        matricula[5] = "381QEV";
-        matricula[6] = "463CVK";
-        matricula[7] = "306RTX";
-
-        String[] marca = new String[8];
-
-        marca[0] = "SEAT";
-        marca[1] = "CITROEN";
-        marca[2] = "JAGUAR";
-        marca[3] = "MITSUBISHI";
-        marca[4] = "FERRARI";
-        marca[5] = "PORSHE";
-        marca[6] = "CADILAC";
-        marca[5] = "NISSAN";
-        marca[7] = "RENAULT";
-
-        String[] modelo = new String[8];
-
-        modelo[0] = "ULTIMATE";
-        modelo[1] = "SPACE";
-        modelo[2] = "MEGANE";
-        modelo[3] = "MONTERO";
-        modelo[4] = "AA3-4";
-        modelo[5] = "THUNDERBOLT";
-        modelo[6] = "LANDCRUISER";
-        modelo[7] = "LAGUNA";
-
-        String[] color = new String[8];
-
-        color[0] = "amarillo";
-        color[1] = "AZUL";
-        color[2] = "BLANCO";
-        color[3] = "PURPURA";
-        color[4] = "NARANJA";
-        color[5] = "ROJO";
-        color[6] = "VERDE";
-        color[7] = "SALMÓN";
-
-        double[] tarifa = new double[8];
-
-        tarifa[0] = 100000.02;
-        tarifa[0] = 40.0000;
-        tarifa[0] = 450.000;
-        tarifa[0] = 7.000;
-        tarifa[0] = 965.000;
-        tarifa[0] = 100.00;
-        tarifa[0] = 187.000;
-        tarifa[0] = 1.0;
-
-        boolean[] disponible = new boolean[8];
-
-        disponible[0] = true;
-        disponible[1] = false;
-        disponible[2] = true;
-        disponible[3] = false;
-        disponible[4] = true;
-        disponible[5] = false;
-        disponible[6] = true;
-        disponible[7] = false;
-
-        Random aleo = new Random();
-
-        int numero = aleo.nextInt(8);
-        //Generamos matricula
-        String matriculaAux = matricula[numero];
-        //generamos marca
-        numero = aleo.nextInt(8);
-        String marcaAux = marca[numero];
-
-        //generamos modelo
-        numero = aleo.nextInt(8);
-        String modeloAux = modelo[numero];
-
-        //generamos color
-        numero = aleo.nextInt(8);
-        String colorAux = color[numero];
-
-        //generamos tarifa
-        double tarifaAux = tarifa[numero];
-
-        //generamos disponibilidad
-        boolean disponibleAux = disponible[numero];
-
-        //Inicializamos el objeto con los valores generados
-        Vehiculo aux = new Vehiculo(matriculaAux, marcaAux, modeloAux, colorAux, tarifaAux, disponibleAux);
-
-        return aux;
-
+  public static Vehiculo vehiculoAleatorio(){
+	 String Matricula = "";
+  	 
+   	 Random rnd = new Random();
+  	 
+   	 for (int i = 0; i < 7; i++) {
+  		 
+   		 if (i < 4) {
+     		 
+       		 Matricula += rnd.nextInt(10);
+   		 }
+   		 else {
+      		 
+       		 Matricula += (char) (rnd.nextInt(90-65+1) + 65);
+   		 }
+   	 }
+   	 String Color = "";
+  	 
+  	 
+  	 
+   	 int numeroColor = rnd.nextInt(9);
+  	 
+   	 switch (numeroColor) {
+   		 case 0:
+       		 Color = "Blanco";
+       		 break;
+   		 case 1:
+       		 Color = "Negro";
+       		 break;
+   		 case 2:
+       		 Color = "Azul";
+       		 break;
+   		 case 3:
+       		 Color = "Amarrillo";
+       		 break;
+   		 case 4:
+       		 Color = "Morado";
+       		 break;
+   		 case 5:
+       		 Color = "Naranja";
+       		 break;
+   		 case 6:
+       		 Color = "Verde";
+       		 break;
+   		 case 7:
+       		 Color = "Rojo";
+       		 break;
+   		 case 8:
+       		 Color = "Cian";
+       		 break;    
+   	 }
+   	 double Tarifa;
+  	 
+  	 
+  	 
+   	 Tarifa =rnd.nextInt(150-100+1) + 100;
+   	 boolean disponibilidad;
+  	 
+      
+  	 
+   	 if (rnd.nextBoolean()){
+   		 disponibilidad = true;
+   	 }else{
+   		 disponibilidad = false;
+   	 }
+	 ArrayList <String> marca = new ArrayList<>();
+    	marca.add("Lexus");
+    	marca.add("Ferrai");
+    	marca.add("Maserrati");
+    	marca.add("Ford");
+    	marca.add("Renault");
+    	marca.add("Seat");
+    	marca.add("Fiat");
+   	 
+    	ArrayList <String> modelo= new ArrayList<>();
+    	modelo.add("Marbella");
+    	modelo.add("Pianto");
+    	modelo.add("RSX300");
+    	modelo.add("Focus");
+    	modelo.add("Clio");
+    	modelo.add("Marbella");
+   
+    
+	 String marcaElegido = marca.get(rnd.nextInt(marca.size()));
+	 String modeloElegido = modelo.get(rnd.nextInt(modelo.size()));
+    
+	 Vehiculo v = new Vehiculo ( Matricula, marcaElegido, modeloElegido,Color, Tarifa , disponibilidad);
+   	 return v;
+  	 
     }
+
 
     @Override
     public String toString() {
         return matricula + "" + marca + "" + modelo + "" + color + "" + tarifa + "" + disponible;
     }
+
+    
 
 }
