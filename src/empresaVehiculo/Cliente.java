11b2ba35bcf72e955400e0570ebe62abb1086de4
gabriel_payano <gabriel21kun@gmail.com>
2019-03-04 16:45:42 +0100

    "Realizamos cambios para adaptar los métodos a las ArrayList"

--- src/empresaVehiculo/Cliente.java
+++ src/empresaVehiculo/Cliente.java
@@ -5,6 +5,7 @@
  */
 package empresaVehiculo;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -53,103 +54,118 @@ public class Cliente {
     }
 
     
-     public static Cliente clienteAleatorio() {
-    	Random rnd = new Random();
+ public static Cliente clienteAleatorio() {
+   	 Random rnd = new Random();
 
-    	String nombre[] = {"Pepe", "Ricardo", "Yoli", "Eduardo", "Manolo", "Felipe"};
-    	String apellido[] = {"Sanchez", "Lopez", "Zubarte", "Payano", "Leon", "Montes"};
+   	 ArrayList <String> nombre = new ArrayList<>();
+   	 
+    	nombre.add("Pepe");
+    	nombre.add("Ricardo");
+    	nombre.add("Yoli");
+    	nombre.add("Eduardo");
+    	nombre.add("Manolo");
+    	nombre.add("Felipe");
+   	 
+    	ArrayList <String> apellido = new ArrayList<>();
+    	apellido.add("Sanchez");
+    	apellido.add("Lopez");
+    	apellido.add("Zubarte");
+    	apellido.add("Payano");
+    	apellido.add("Leon");
+    	apellido.add("Montes");
 
-    	String nifrandom = "";
-    	int dni[] = new int[8];
-    	for (int i = 0; i <= dni.length - 1; i++) {
-        	dni[i] = rnd.nextInt(10);
+   	 String nifrandom = "";
+   	 ArrayList<Integer> dni = new ArrayList<>();
+   	 for (int i = 0; i <= 8- 1; i++) {
+   		 dni.add(rnd.nextInt(10));
 
-    	}
-    	String letra = "";
-    	int i = 0;
-    	for (int value : dni) {
-        	letra += String.valueOf(value);
-    	}
-    	int calculoLetra = Integer.parseInt(letra);
-    	int c = calculoLetra % 23;
-    	switch (c) {
-        	case 0:
-            	letra += "T";
-            	break;
-        	case 1:
-            	letra += "R";
-            	break;
-        	case 2:
-            	letra += "W";
-            	break;
-        	case 3:
-            	letra += "A";
-            	break;
-        	case 4:
-            	letra += "G";
-            	break;
-        	case 5:
-            	letra += "M";
-            	break;
-        	case 6:
-            	letra += "Y";
-            	break;
-        	case 7:
-            	letra += "F";
-            	break;
-        	case 8:
-            	letra += "P";
-            	break;
-        	case 9:
-            	letra += "D";
-            	break;
-        	case 10:
-            	letra += "X";
-            	break;
-        	case 11:
-            	letra += "B";
-            	break;
-        	case 12:
-            	letra += "N";
-            	break;
-        	case 13:
-            	letra += "J";
-            	break;
-        	case 14:
-            	letra += "Z";
-            	break;
-        	case 15:
-            	letra += "S";
-            	break;
-        	case 16:
-            	letra += "Q";
-            	break;
-        	case 17:
-            	letra += "V";
-            	break;
-        	case 18:
-            	letra += "H";
-            	break;
-        	case 19:
-            	letra += "L";
-            	break;
-        	case 20:
-            	letra += "C";
-            	break;
-        	case 21:
-            	letra += "K";
-            	break;
-        	case 22:
-            	letra += "E";
-            	break;
+   	 }
+   	 String letra = "";
+   	 int i = 0;
+   	 for (int value : dni) {
+   		 letra += String.valueOf(value);
+   	 }
+   	 int calculoLetra = Integer.parseInt(letra);
+   	 int c = calculoLetra % 23;
+   	 switch (c) {
+   		 case 0:
+       		 letra += "T";
+       		 break;
+   		 case 1:
+       		 letra += "R";
+       		 break;
+   		 case 2:
+       		 letra += "W";
+       		 break;
+   		 case 3:
+       		 letra += "A";
+       		 break;
+   		 case 4:
+       		 letra += "G";
+       		 break;
+   		 case 5:
+       		 letra += "M";
+       		 break;
+   		 case 6:
+       		 letra += "Y";
+       		 break;
+   		 case 7:
+       		 letra += "F";
+       		 break;
+   		 case 8:
+       		 letra += "P";
+       		 break;
+   		 case 9:
+       		 letra += "D";
+       		 break;
+   		 case 10:
+       		 letra += "X";
+       		 break;
+   		 case 11:
+       		 letra += "B";
+       		 break;
+   		 case 12:
+       		 letra += "N";
+       		 break;
+   		 case 13:
+       		 letra += "J";
+       		 break;
+   		 case 14:
+       		 letra += "Z";
+       		 break;
+   		 case 15:
+       		 letra += "S";
+       		 break;
+   		 case 16:
+       		 letra += "Q";
+       		 break;
+   		 case 17:
+       		 letra += "V";
+       		 break;
+   		 case 18:
+       		 letra += "H";
+       		 break;
+   		 case 19:
+       		 letra += "L";
+       		 break;
+   		 case 20:
+       		 letra += "C";
+       		 break;
+   		 case 21:
+       		 letra += "K";
+       		 break;
+   		 case 22:
+       		 letra += "E";
+       		 break;
 
-    	}
+   	 }
 
-    	String nombreElegido = nombre[rnd.nextInt(nombre.length)];
-    	String apellidoElegido = apellido[rnd.nextInt(apellido.length)];
-    	Cliente cliente = new Cliente(letra,nombreElegido,apellidoElegido);
-    	return cliente;
-	}
+   	 String nombreElegido = nombre.get(rnd.nextInt(nombre.size()));
+   	 String apellidoElegido = apellido.get(rnd.nextInt(apellido.size()));
+   	 Cliente cliente = new Cliente(letra,nombreElegido,apellidoElegido);
+   	 return cliente;
+    }
+
      
      
 
